d7876462d463a5da023823bb7df4636f42c66685
Steven Bever <sbever@knightpoint.com>
2020-12-10 11:24:18 -0500

    completed part 2

--- Day8/handheld_halting.rb
+++ Day8/handheld_halting.rb
@@ -18,16 +18,20 @@ class Instruction
 end
 
 
-def process_instruction_file file_path
+def process_instruction_set instructions
   c = 0
   accumulator=0
-  instructions = Instruction.parse_instruction_file(file_path)
 
   instructions_processed = []
   current_index = 0
+  finished_instructions = false
   while true
-    break if instructions_processed.include? current_index
-    break if current_index < 0 || current_index >= instructions.length
+    break if instructions_processed.include? current_index || current_index < 0
+    if current_index >= instructions.length
+      finished_instructions = true
+      break
+    end
+    instructions_processed << current_index
     case instructions[current_index].instruction
     when 'nop'
       current_index +=1
@@ -41,13 +45,31 @@ def process_instruction_file file_path
     end
   end
   
-  return current_index, accumulator
+  return current_index, accumulator, finished_instructions
 end
 
-part_one_last_index, part_one_accumulator = process_instruction_file'./input.txt'
+instructions = Instruction.parse_instruction_file('./input.txt')
+part_one_last_index, part_one_accumulator, finished_instructions = process_instruction_set instructions
 puts "Last instruction index ran on bad instruction set #{part_one_last_index}"
 puts "DAY 8 Part 1: Accumulator value at loop start: #{part_one_accumulator}" #1816
 
-part_two_last_index, part_two_accumulator = process_instruction_file'./input_fixed.txt'
+
+updated_instruction = 0
+while updated_instruction < instructions.length
+  if  instructions[updated_instruction].instruction == 'nop'
+    instructions[updated_instruction].instruction = 'jmp'
+    part_two_last_index, part_two_accumulator, part_two_finished_instructions = process_instruction_set instructions
+    puts 'fin 2' if part_two_finished_instructions
+    break if part_two_finished_instructions
+    instructions[updated_instruction].instruction = 'nop'
+  elsif  instructions[updated_instruction].instruction == 'jmp'
+    instructions[updated_instruction].instruction = 'nop'
+    part_two_last_index, part_two_accumulator, part_two_finished_instructions = process_instruction_set instructions
+    break if part_two_finished_instructions
+    instructions[updated_instruction].instruction = 'jmp'
+  end
+  updated_instruction += 1
+end
+
 puts "Last instruction index ran on fixed instruction set #{part_two_last_index}"
-puts "DAY 8 Part 2: Accumulator value at end of program: #{part_two_accumulator}"
+puts "DAY 8 Part 2: Accumulator value at end of program: #{part_two_accumulator}"#1149
